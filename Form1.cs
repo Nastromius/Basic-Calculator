92c4a1804f2a275150e47130382a718f22f7ddea
Vladimir <86318922+Nastromius@users.noreply.github.com>
2021-06-25 20:33:46 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Calculator
+{
+    public partial class Form1 : Form
+    {
+        float a, b;
+        int count;
+        bool sign = true;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.KeyDown += TextBox1_KeyDown;
+
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad0:
+                    button16.PerformClick();
+                    break;
+                case Keys.NumPad1:
+                    button13.PerformClick();
+                    break;
+                case Keys.NumPad2:
+                    button14.PerformClick();
+                    break;
+                case Keys.NumPad3:
+                    button15.PerformClick();
+                    break;
+                case Keys.NumPad4:
+                    button9.PerformClick();
+                    break;
+                case Keys.NumPad5:
+                    button10.PerformClick();
+                    break;
+                case Keys.NumPad6:
+                    button11.PerformClick();
+                    break;
+                case Keys.NumPad7:
+                    button5.PerformClick();
+                    break;
+                case Keys.NumPad8:
+                    button6.PerformClick();
+                    break;
+                case Keys.NumPad9:
+                    button7.PerformClick();
+                    break;
+                case Keys.Multiply:
+                    button4.PerformClick();
+                    break;
+                case Keys.Add:
+                    button18.PerformClick();
+                    break;
+                case Keys.Subtract:
+                    button8.PerformClick();
+                    break;
+                case Keys.Divide:
+                    button3.PerformClick();
+                    break;
+            }
+        }
+
+        private void calculate()
+        {
+            switch (count)
+            {
+                case 1:
+                    b = a + float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+                case 2:
+                    b = a - float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+                case 3:
+                    b = a * float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+                case 4:
+                    float check;
+                    check = float.Parse(textBox1.Text);
+                    if (check == 0.0)
+                        MessageBox.Show("Внимание! Деление на ноль!");
+                    else
+                    {
+                        b = a / check;
+                        textBox1.Text = b.ToString();
+                    }
+                    break;
+                
+
+                default:
+                    break;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            label1.Text = a.ToString() + "+";
+            sign = true;
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 2;
+            label1.Text = a.ToString() + "-";
+            sign = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            label1.Text = a.ToString() + "*";
+            sign = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            label1.Text = a.ToString() + "/";
+            sign = true;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            calculate();
+            label1.Text = "";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int length = textBox1.Text.Length - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < length; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if (sign == true)
+            {
+                textBox1.Text = "-" + textBox1.Text;
+                sign = false;
+            }
+
+            else if (sign == false)
+            {
+                textBox1.Text = textBox1.Text.Replace("-", "");
+                sign = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ",";
+        }
+
+    }
+}
